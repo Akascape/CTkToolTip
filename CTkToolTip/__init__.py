e2dc0bc0328e11e38fcf5ae3ad6660d54de9707e
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-04-11 13:19:57 +0530

    Fixed the widget destroy issue

--- CTkToolTip/__init__.py
+++ CTkToolTip/__init__.py
@@ -5,7 +5,7 @@ This is a tooltip/pop-up widget made with customtkinter.
 Homepage: https://github.com/Akascape/CTkToolTip
 """
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 from .ctk_tooltip import CTkToolTip
 
